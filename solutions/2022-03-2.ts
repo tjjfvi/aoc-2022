e8970d9ab0aeaf499a13c19ae8c4ccb2fc931277
tjjfvi <t6@t6.fyi>
2022-12-03 07:25:33 -0500

    day 3

---
+++ solutions/2022-03-2.ts
@@ -0,0 +1,57 @@
+import { aoc, solution, test } from "../host.ts";
+import { _, add, allGens, arr, asc, dbg, dsc, groupBy, mult, raceGens, repeatTillConst, toNum } from "./helpers.ts";
+
+aoc(2022, 3, 2);
+
+// prettier-ignore
+test`
+
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+
+
+`({
+  part1: 157,
+  part2: 70,
+});
+
+// prettier-ignore
+test`
+
+`({
+  part1: -1,
+  part2: -1,
+});
+
+// prettier-ignore
+test`
+
+`({
+  part1: -1,
+  part2: -1,
+});
+
+solution(async (input) => {
+  dbg.x(input);
+
+  const elves = input.split("\n").map((x) =>
+    x.split("").map((x) => " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(x))
+  );
+
+  return dbg(
+    elves.map((x, i, a) => a.slice(i, i + 3)).filter((x, i, a) => i % 3 === 0).map((x) =>
+      dbg(x[0].filter((a) => x[1].includes(a) && x[2].includes(a)))[0]!
+    ).reduce(add, 0),
+  );
+
+  // .map((x) => [x.slice(0, x.length / 2), x.slice(x.length / 2)]).map(([a, b]) =>
+  //   dbg(a.filter((x) => b.includes(x)))[0]
+  // )
+  //   .reduce(add, 0);
+
+  return 0;
+});
